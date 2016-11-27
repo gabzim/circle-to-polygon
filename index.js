84bbb856238657e19e3b0c9ba6faded095dabae2
Gabriel Zimmermann <gabriel.zimmermann@rw.co>
2016-11-27 17:24:23 -0300

    Gabriel Zimmermann - Initial Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+"use strict";
+function toRadians(angleInDegrees) {
+  return angleInDegrees * Math.PI / 180;
+}
+
+function toDegrees(angleInRadians) {
+  return angleInRadians * 180 / Math.PI;
+}
+
+function offset(c1, distance, bearing) {
+  var lat1 = toRadians(c1[1]);
+  var lon1 = toRadians(c1[0]);
+  var dByR = distance / 6378137; // distance divided by 6378137 (radius of the earth) wgs84
+  var lat = Math.asin(
+    Math.sin(lat1) * Math.cos(dByR) +
+    Math.cos(lat1) * Math.sin(dByR) * Math.cos(bearing));
+  var lon = lon1 + Math.atan2(
+      Math.sin(bearing) * Math.sin(dByR) * Math.cos(lat1),
+      Math.cos(dByR) - Math.sin(lat1) * Math.sin(lat));
+  return [toDegrees(lon), toDegrees(lat)];
+}
+
+module.exports = function circleToPolygon(center, radius, numberOfSegments) {
+  var n = numberOfSegments ? numberOfSegments : 32;
+  var flatCoordinates = [];
+  var coordinates = [];
+  for (let i = 0; i < n; ++i) {
+    flatCoordinates.push.apply(flatCoordinates, offset(center, radius, 2 * Math.PI * i / n));
+  }
+  flatCoordinates.push(flatCoordinates[0], flatCoordinates[1]);
+
+  for (let i = 0, j = 0; j < flatCoordinates.length; j += 2) {
+    coordinates[i++] = flatCoordinates.slice(j, j + 2);
+  }
+
+  return {
+    type: 'Polygon',
+    coordinates: [coordinates]
+  };
+};

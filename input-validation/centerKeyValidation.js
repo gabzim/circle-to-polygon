c96f85a51baf0f324d2ca742bc76e105559e164c
Johannes Jarbratt <johannes.jarbratt+github@gmail.com>
2020-02-02 17:53:30 +0900

    Passing Tests

--- input-validation/centerKeyValidation.js
+++ input-validation/centerKeyValidation.js
@@ -1,21 +1,22 @@
 exports.centerKeyValidation = function centerKeyValidation(center) {
-  function objHasProp(obj, prop) {
-    // .hasOwnProperty is not a protected method name
-    return Object.prototype.hasOwnProperty.call(obj, prop);
-  }
-
-  if (
-    objHasProp(center, "lat") &&
-    !(objHasProp(center, "lon") || objHasProp(center, "lng"))
-  ) {
+  if (objHasProp(center, "lat") && !hasLonOrLngKey(center)) {
     throw new Error(
       "ERROR! Longitude has to be passed with key 'lon' or 'lng' when latiude is passed with key 'lat'"
     );
   }
 
-  if (objHasProp(center, "lon") && !objHasProp(center, "lat")) {
+  if (hasLonOrLngKey(center) && !objHasProp(center, "lat")) {
     throw new Error(
       "ERROR! Latitude has to be passed with key 'lat' when longitude is passed with key 'lon' or 'lng'"
     );
   }
 };
+
+function hasLonOrLngKey(obj) {
+  return objHasProp(obj, "lon") || objHasProp(obj, "lng");
+}
+
+function objHasProp(obj, prop) {
+  // .hasOwnProperty is not a protected method name
+  return Object.prototype.hasOwnProperty.call(obj, prop);
+}

49f32da2d3fd30a0e317c9145ad48b74aab69dc7
Johannes Jarbratt <johannes.jarbratt+github@gmail.com>
2020-02-02 15:17:00 +0900

    Refa. Moved Input Validation

---
+++ input-validation/validateCenter.js
@@ -0,0 +1,23 @@
+exports.validateCenter = function validateCenter(center) {
+  const validCenterLengths = [2, 3]
+  if (!Array.isArray(center) || !validCenterLengths.includes(center.length)) {
+    throw new Error("ERROR! Center has to be an array of length two or three");
+  }
+  const [lng, lat] = center;
+  if (typeof lng !== "number" || typeof lat !== "number") {
+    throw new Error(
+      `ERROR! Longitude and Latitude has to be numbers but where ${typeof lng} and ${typeof lat}`
+    );
+  }
+  if (lng > 180 || lng < -180) {
+    throw new Error(
+      `ERROR! Longitude has to be between -180 and 180 but was ${lng}`
+    );
+  }
+
+  if (lat > 90 || lat < -90) {
+    throw new Error(
+      `ERROR! Latitude has to be between -90 and 90 but was ${lat}`
+    );
+  }
+}

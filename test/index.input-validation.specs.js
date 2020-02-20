0ed367940490906e470769015bcb8da5beed7e1c
Johannes Jarbratt <johannes.jarbratt+github@gmail.com>
2020-02-20 23:04:56 +0900

    Task: Test Refactor

--- test/index.input-validation.specs.js
+++ test/index.input-validation.specs.js
@@ -149,24 +149,38 @@ describe("Input verification", () => {
       );
     });
 
-    it("should throw error on too low value of numberOfSegments", () => {
-      assert.throws(
-        () => circleToPolygon([-59.99029, -58.99029], 50, 0),
-        Error,
-        "ERROR! Number of segments has to be at least 3 but was: 0"
-      );
+    describe("On too low numberOfSegments value", () => {
+      it("numberOfSegments is < 0", () => {
+        assert.throws(
+          () => circleToPolygon([-59.99029, -58.99029], 50, -10),
+          Error,
+          "ERROR! Number of segments has to be at least 3 but was: -10"
+        );
+      });
 
-      assert.throws(
-        () => circleToPolygon([-59.99029, -58.99029], 50, 1),
-        Error,
-        "ERROR! Number of segments has to be at least 3 but was: 1"
-      );
+      it("numberOfSegments is zero (0)", () => {
+        assert.throws(
+          () => circleToPolygon([-59.99029, -58.99029], 50, 0),
+          Error,
+          "ERROR! Number of segments has to be at least 3 but was: 0"
+        );
+      });
+  
+      it("numberOfSegments is one (1)", () => {
+        assert.throws(
+          () => circleToPolygon([-59.99029, -58.99029], 50, 1),
+          Error,
+          "ERROR! Number of segments has to be at least 3 but was: 1"
+        );
+      });
 
-      assert.throws(
-        () => circleToPolygon([-59.99029, -58.99029], 50, 2),
-        Error,
-        "ERROR! Number of segments has to be at least 3 but was: 2"
-      );
-    });
+      it("numberOfSegments is 2 (2)", () => {
+        assert.throws(
+          () => circleToPolygon([-59.99029, -58.99029], 50, 2),
+          Error,
+          "ERROR! Number of segments has to be at least 3 but was: 2"
+        );
+      });
+    })
   });
 });

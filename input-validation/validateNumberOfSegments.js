25657430407270172e1118c0f8f7a1576b4a2ca4
Johannes Jarbratt <johannes.jarbratt+github@gmail.com>
2020-02-20 23:11:11 +0900

    Test: Throw Error if numberOfEdges is an Array

--- input-validation/validateNumberOfSegments.js
+++ input-validation/validateNumberOfSegments.js
@@ -2,8 +2,11 @@ exports.validateNumberOfSegments = function validateNumberOfSegments(
   numberOfSegments
 ) {
   if (typeof numberOfSegments !== "number") {
+    const ARGUMENT_TYPE = Array.isArray(numberOfSegments)
+      ? "array"
+      : typeof numberOfSegments;
     throw new Error(
-      `ERROR! Number of segments has to be a number but was: ${typeof numberOfSegments}`
+      `ERROR! Number of segments has to be a number but was: ${ARGUMENT_TYPE}`
     );
   }
 
